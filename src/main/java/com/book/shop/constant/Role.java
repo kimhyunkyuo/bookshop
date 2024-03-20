834f443415e53e37105e4d193f496cb417b140fa
hyunkyu.kim <sdr04076@naver.com>
2024-03-21 08:57:19 +0900

    Feat: 회원가입 완료(카카와 API 주소)       스프링 시큐리티 적용

---
+++ src/main/java/com/book/shop/constant/Role.java
@@ -0,0 +1,6 @@
+package com.book.shop.constant;
+
+public enum Role {
+    USER, ADMIN
+
+}
---
+++ src/main/java/com/book/shop/entity/Member.java
@@ -0,0 +1,55 @@
+package com.book.shop.entity;
+
+import com.book.shop.constant.Role;
+import com.book.shop.dto.MemberFormDto;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.lang.model.element.Name;
+
+@Entity
+@Table(name = "member")
+@Getter@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String zipcode;
+
+    private String streetadr;
+
+    private String detailadr;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setZipcode(memberFormDto.getZipcode());
+        member.setDetailadr(memberFormDto.getDetailadr());
+        member.setStreetadr(memberFormDto.getStreetadr());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.ADMIN);
+        return member;
+    }
+
+}
