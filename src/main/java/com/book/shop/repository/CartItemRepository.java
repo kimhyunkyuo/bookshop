eb73ad8176edf093170910507be89e1057df1e05
hyunkyu.kim <sdr04076@naver.com>
2024-03-27 17:08:11 +0900

    Feat: 장바구니 담기 구현

---
+++ src/main/java/com/book/shop/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.book.shop.repository;
+
+import com.book.shop.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+}
